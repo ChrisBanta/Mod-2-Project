720c4df996ec1a15029e15d51409e1fcbbdd7f0c
Ben Rowe <brow495@gmail.com>
2020-06-17 08:16:50 -0600

    Add first question seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Superhero.destroy_all
+Question.destroy_all
 
 
 
@@ -16,3 +17,36 @@ for i in(1..10) do
 
     
 end  
+
+Question.create(number: 1, 
+                text: "Who wins in a race?", 
+                formula: Proc.new{|players| players.map(&:speed).max})
+                
+Question.create(number: 2, 
+                text: "Who would you want as a trivia partner?", 
+                formula: Proc.new{|players| players.map(&:intelligence).max})
+
+Question.create(number: 3, 
+                text: "If you were to invest in the future of one of these heroes, which would it be?", 
+                formula: Proc.new{|players| (players.map(&:durability) + players.map(&:intelligence)).max})
+
+Question.create(number: 4, 
+                text: "If a bear attacked you, who would you want by your side?", 
+                formula: Proc.new{|players| players.map(&:speed).min})
+
+Question.create(number: 5, 
+                text: "If you needed to, let’s say, leap a tall building in a single bound, who could help you with that?", 
+                formula: Proc.new{|players| players.map(&:strength)})
+
+# Question.create(number: 6, text: , formula: )
+# Question.create(number: 7, text: , formula: )
+# Question.create(number: 8, text: , formula: )
+# Question.create(number: 9, text: , formula: )
+# Question.create(number: 10, text: , formula: )
+# Question.create(number: 11, text: , formula: )
+# Question.create(number: 12, text: , formula: )
+# Question.create(number: 13, text: , formula: )
+# Question.create(number: 14, text: , formula: )
+# Question.create(number: 15, text: , formula: )
+# Question.create(number: 16, text: , formula: )
+# Question.create(number: 17, text: , formula: )
